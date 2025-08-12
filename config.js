1cabbf60652fadb9623721b154418ce76adc2dd2
uzizizah <Cyrilflandre@outlook.fr>
2025-08-12 13:45:41 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
         prefixe: '+', // Préfixe - Le texte avant les commandes
         buyer: '915321015515643935', // Buyer
         couleur: '#6495ED', // Couleur HEX
-        footer: 'Montignac RP', // Dans la commande help ou autre il y'a un texte en bas
+        footer: 'Lille Gestion', // Dans la commande help ou autre il y'a un texte en bas
         maxServer: '2', // Le nombre de serveur maximum que le bot peut rejoindre 
     }
 }
